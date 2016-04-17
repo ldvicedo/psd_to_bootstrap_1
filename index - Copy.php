d5ff34c7b08867a73b09dba2683534f0ae83971c
ldvicedo <lyndon.vicedo@gmail.com>
2016-04-17 15:32:46 +0800

    Updated sections

---
+++ index - Copy.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tajam</title>
+<meta charset="UTF-8">
+<meta name="description" content="PSD to Bootstrap">
+<meta name="keywords" content="Practice">
+<meta name="author" content="Lyndon Dennis Vicedo">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<div id="header">
+	<!-- nav -->
+	<div class="container">
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo" class="comp-logo" /></a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav navbar-right">
+				<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Expertise</a></li>
+				<li><a href="#">Teams</a></li>
+				<li><a href="#">Works</a></li>
+				<li><a href="#">People Say</a></li>
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div><!-- end nav -->
+	<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">	
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner carousel-inner-height" role="listbox">
+		<div class="item active">
+		  <div class="container" style="padding-top: 200px; color: #fff;">
+			  <!--img src="img_chania.jpg" alt="Chania"-->
+				<h1>We Are Awesome Creative Agency</h1>
+				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+		  </div>
+		</div>
+
+		<div class="item">
+			<div class="container" style="padding-top: 200px; color: #fff;">
+			  <!--img src="img_chania.jpg" alt="Chania"-->
+				<h1>We Are Awesome Creative Agency</h1>
+				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+			</div>
+		</div>
+
+		<div class="item">
+			<div class="container" style="padding-top: 200px; color: #fff;">
+			  <!--img src="img_chania.jpg" alt="Chania"-->
+				<h1>We Are Awesome Creative Agency</h1>
+				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+			</div>
+		</div>
+
+		<div class="item">
+			<div class="container" style="padding-top: 200px; color: #fff;">
+				<h1>We Are Awesome Creative Agency</h1>
+				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+			</div>
+		</div>
+	</div>
+
+	  <!-- Left and right controls -->
+	  <!-- 
+	  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	  </a>
+	  -->
+	</div>
+</div>
+<div class="section-1">
+	<div class="container">
+		<p>test</p>
+	</div>
+</div>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html> 
